978f6b3999de97092456378fe108eb9440074bbb
Baskar-B10 <baskareie39@gmail.com>
2025-11-27 21:21:38 +0530

    Update TrackerDetailsApplication.java

--- src/main/java/com/example/trackerdetails/TrackerDetailsApplication.java
+++ src/main/java/com/example/trackerdetails/TrackerDetailsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrackerDetailsApplication {
     public static void main(String[] args) {
-         System.out.println("MONGO_URI = " + System.getenv("MONGO_URI")); 
         SpringApplication.run(TrackerDetailsApplication.class, args);
     }
 }
